74fa98e8ca7663bc2baaf73261021516e2c3dec7
KartoonYoko <crashed9001@gmail.com>
2020-02-24 14:26:12 +0900

    asdasd

--- Lab 2/bot.py
+++ Lab 2/bot.py
@@ -1,50 +1,185 @@
 import datetime
+import re
+import json
+import requests
+
 
 class Bot:
 	""" Класс чат-бот.
 		Умеет овечать на
 			реплики-шаблолны:
 				- Привет, бот!
-				-
-				-
-				-
-				-
+				- В чем смысл жизни?
 			простые команды:
 				- Который час?
-				-
-				-
+				- Сколько времени?
+				- Какой год?
 			команды с параметрами:
-				- умножь 12 на 157
-				- 
-				-
+				- Умножь 12 на 157
+				- Сложи 14 и 13
+				- Раздели 67 на 4
+				- Прибавь 78 к 87
 		Умеет хранить историю сообщений, записывая историю в файл по завершению программы.
 		И загружать из файла при запуске.
 
 		Получение актуальной информации из интернета.
 	"""
 
+	def find_out_condition(self, condition):
+		""" Перевести описание погоды для метода find_out_weather"""
+		if condition == "clear":
+			return "ясно"
+		elif condition == "partly-cloudy":
+			return "малооблачно"
+		elif condition == "cloudy":
+			return "облачно с прояснениями"
+		elif condition == "overcast":
+			return "пасмурно"
+		elif condition == "partly-cloudy-and-light-rain":
+			return "небольшой дождь"
+		elif condition == "partly-cloudy-and-rain":
+			return "дождь"
+		elif condition == "overcast-and-rain":
+			return "сильный дождь"
+		elif condition == "overcast-thunderstorms-with-rain":
+			return "сильный дождь, гроза"
+		elif condition == "cloudy-and-light-rain":
+			return "небольшой дождь"
+		elif condition == "overcast-and-light-rain":
+			return "небольшой дождь"
+		elif condition == "cloudy-and-rain":
+			return "дождь"
+		elif condition == "overcast-and-wet-snow":
+			return "дождь со снегом"
+		elif condition == "partly-cloudy-and-light-snow":
+			return "небольшой снег"
+		elif condition == "partly-cloudy-and-snow":
+			return "снег"
+		elif condition == "overcast-and-snow":
+			return "снегопад"
+		elif condition == "cloudy-and-light-snow":
+			return "небольшой снег"
+		elif condition == "overcast-and-light-snow":
+			return "небольшой снег"
+		elif condition == "cloudy-and-snow":
+			return "снег"
+		else:
+			return "Непонятные условия погоды"
 
-	def __init__(self, userName = "user"):
-		self._userName = userName
+	def find_out_weather(self):
+		""" Запрос погоды через Yandex API"""
+		api_key = "ae76725b-fa0a-4b4e-a41b-38e5e8d03037"
+		# широта и долгота нужного города (для Читы "52.033973" и "113.499432")
+		lat = "52.033973"
+		lon = "113.499432"
+		try:
+			headers = {'X-Yandex-API-Key': api_key}
+			res = requests.get("https://api.weather.yandex.ru/v1/forecast?lat={}&lon={}".format(lat, lon), headers=headers)
+			print(res.status_code)
+			data = res.json()
+			stroka = find_out_condition(str(data["fact"]['condition']))
+			answer = "Температура (°C): " + str(data["fact"]["temp"]) + \
+					 "\nОщущается как: " + str(data["fact"]['feels_like']) + \
+					 "\n" + stroka# + \
+					# "\nВетер: " + str(data["fact"]['wind_speed'])
+			return answer
+		except Exception as e:
+			return "Что-то пошло не так." + e
 
+	def get_history_of_chatting(self):
+		return self._rec
+
+	def write_to_json(self):
+		""" Запись в файл"""
+		with open("users/"+self._userName+".json", "w") as file:
+			json.dump(self._rec, file, indent="")
+
+	def read_from_json(self):
+		""" Чтение из файла с именем пользователя"""
+		try:
+			with open("users/"+self._userName+".json", "r") as file:
+				self._rec = json.load(file)
+		except FileNotFoundError:
+			self._rec = {}
+
+	def fill_dict(self, key, data):
+		""" ЗАполнить запись"""
+		self._rec[key] = data
+
+	def __init__(self, user_name="user"):
+		""" Конструктор для инициализации имени пользователя"""
+		self._userName = user_name  # Имя пользователя
+		self._rec = {}
+		self.read_from_json()  # инициализируется свойство _rec для хранения истории переписки в виде словаря
 
 	def reply(self, s):
+		""" Метод "ответа бота". Вернет строку с ответом. """
 		if isinstance(s, str):
+			answer = 0  # для ответа на арифметические вопросы
+			now = datetime.datetime.now()  # текущее время
 			s = s.lower()
-			if "привет, бот" in s:
-				return str("Привет, " + self._userName)
-			elif "который час?" in s:
-				now = datetime.datetime.now()			# 
-				return now.strftime("%d-%m-%Y %H:%M")	# Вернет текущую дату и время
-			elif "умножь" in s:
-				buf = s.find("на", s.find("умножь"))
-				if buf != -1:
-					s1 = s[s.find("умножь") + 1: buf - 1]
-					s2 = s[buf + 1: s.find(" ", buf + 1) - 1]
-					try:
-						answer = s1 * s2
-					except ValueError as e:
-						return "Неверный формат"
-					return str(answer)
-			else:
-				return "Я Вас не понял, не могли бы повторить?"
+			message = "Я Вас не понял, не могли бы Вы повторить?"
+
+			# Приветствие в ответ на "Привет бот"
+			match = re.search(r'(?:пр\w{,1}в\w{,1}т\w{,1}\s{,10}б\w{,2}т)', s)
+			if match:
+				message = "Привет, " + self._userName + "!"
+
+			# шаблонное выражение для вывода времени
+			match = re.search(r'(?:ск\w{,1}л\w{,1}к\w{,1}\s{,10}вр\w{,1}м\w{,3}|к\w{,1}т\w{,1}рый\s{,10}ч\w{,1}с)', s)
+			if match:
+				message = "Сейчас "+now.strftime("%H:%M")   # Вернет текущее время
+			match = re.search(r'(?:к\w{,1}к\w{,1}й(?:\s{,10}|\s{,10}сейчас\s{,10})г\w{,1}д)|(?:к\w{,1}к\w{,1}ое\s{,10}ч\w{,1}сл\w{,1})', s)
+			if match:
+				message = now.strftime("%d.%m.%Y")  # Вернет текущую дату
+
+			# шаблонное выражение для умножения
+			match = re.search(r'(?:по|у)множ\w+\s{,10}(\d{1,10})\s{,10}(?:на|н\w{,1}|\w{,1}а|и)\s{,10}(\d{1,10})', s)
+			if match:
+				s1 = float(match[1])
+				s2 = float(match[2])
+				try:
+					answer = s1 * s2
+				except:
+					message = "Неверный формат"
+				message = str(answer)
+
+			# шаблонное выражение для сложения
+			match = re.search(r'(?:сложи|прибавь)\s{,10}(\d{1,10})\s{,10}(?:и|к)\s{,10}(\d{1,10})', s)
+			if match:
+				s1 = float(match[1])
+				s2 = float(match[2])
+				try:
+					answer = s1 + s2
+				except:
+					message = "Неверный формат"
+				message = str(answer)
+
+			# шаблонное выражение для вычитания
+			match = re.search(r'(?:вычти)\s{,10}(\d{1,10})\s{,10}(?:из)\s{,10}(\d{1,10})', s)
+			if match:
+				s1 = float(match[1])
+				s2 = float(match[2])
+				try:
+					answer = s2 - s1
+				except:
+					message = "Неверный формат"
+				message = str(answer)
+
+			# шаблонное выражение для деления
+			match = re.search(r'(?:раздели)\s{,10}(\d{1,10})\s{,10}(?:на)\s{,10}(\d{1,10})', s)
+			if match:
+				s1 = float(match[1])
+				s2 = float(match[2])
+				try:
+					answer = s1 / s2
+				except:
+					message = "Неверный формат"
+				message = str(answer)
+			# Что есть смысл?
+			match = re.search(r'(?:что есть смысл жизни|в чем смысл|в чем смысл жизни)', s)
+			if match:
+				message = "42"
+
+			self.fill_dict(now.strftime("%d.%m.%Y %H:%M:%S"), [s, message])
+			return message
--- Lab 2/main.py
+++ Lab 2/main.py
@@ -1,8 +1,12 @@
 from bot import Bot
 
-user = input("Консоль для работы с чат-ботом. Ввведите ваше имя.")
+
+# user = input("Консоль для работы с чат-ботом. Ввведите ваше имя.")
+user = "asdasd"
 alex = Bot(user)
-x = input("Теперь можете написать ему что-нибудь.")
-while x != "Yes":
-	print(alex.reply(x))
-	x = input("Для завершения введите Yes.")
+# x = input("Теперь можете написать ему что-нибудь.")
+# print(alex.get_history_of_chatting())
+# while x != "q":
+# 	print(alex.reply(x))
+# 	x = input("Для завершения введите \"q\".")
+print(alex.find_out_weather())
